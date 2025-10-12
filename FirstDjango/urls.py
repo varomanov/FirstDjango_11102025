c6a925a146c70d81a5379f993fc476343a462ffb
Vladimir Romanov <vladimirromanovmsk@gmail.com>
2025-10-12 10:47:26 +0300

    Done.M1:Tasks

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -4,4 +4,6 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
+    path('item/<int:id>', views.item),
+    path('items', views.items)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -11,6 +11,14 @@ person = {
     'email': 'vasya@mail.ru',
 }
 
+products = [
+    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+    {"id": 7, "name": "Картофель фри", "quantity": 0},
+    {"id": 8, "name": "Кепка", "quantity": 124},
+]
+
 
 def home(request):
     text = """
@@ -29,4 +37,30 @@ def about(request):
         <p>телефон: {person['phone']}</p>
         <p>email: {person['email']}</p>
     </div>"""
+    return HttpResponse(text)
+
+
+def item(request, id: int):
+    lstIds = [x['id'] for x in products]
+    if id in lstIds:
+        product = [x for x in products if x['id'] == id][0]
+        text = f"""
+        <p style='font-weight: bold;'>Product information:</p>
+        <ul>
+            <li>Name: {product['name']}</li>
+            <li>Quantity: {product['quantity']}</li>
+        </ul>
+        <a href='/items'>Back to product list</a>
+        """
+        return HttpResponse(text)
+    return HttpResponse('Товар не найден')
+
+
+def items(request):
+    list_items = ""
+    for i in products:
+        list_items += f'<li><a href="item/{i['id']}">{i}</a></li>'
+    text = f"""
+    <h2>All products</h2>
+    <ol>{list_items}</ol>"""
     return HttpResponse(text)
